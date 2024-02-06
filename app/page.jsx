51c4349f3b57dc95c1edea4316785a53e1169e10
mdabdulaziztanvir <abdulazizztanvir@gmail.com>
2024-02-06 19:43:59 +0600

    tets

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <> <h1>This is home</h1> </>
+    <> <h1>This is home</h1> 
+    <h2 className="text-9xl" >FRN</h2>
+    </>
   );
 }
